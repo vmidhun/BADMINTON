4ded34c70f6b9bcad725ffb3297831853d031ef3
midhun-neo <midhun@neointeraction.com>
2021-10-30 17:10:00 +0530

    Player Add Complete

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -4,17 +4,19 @@ import { Injectable } from '@angular/core';
 export class PLAYER {
   'ID': number;
   'Name': string;
-  'Age': number;
+  'DoB': number;
   'Gender': string;
   'Flat': string;
 }
 
-  let players: Players[] = [
-    {ID: 1, Name: 'Midhun', Age: 41, Gender: "M", Flat: "B4514"},
-    {ID: 2, Name: 'Anil', Age: 41, Gender: "M", Flat: "B4514"},
-    {ID: 3, Name: 'Chandru', Age: 41, Gender: "M", Flat: "B4514"},
-    {ID: 4, Name: 'Chetan', Age: 41, Gender: "M", Flat: "B4514"},
-    {ID: 5, Name: 'Aniket', Age: 42, Gender: "M", Flat: "B4514"}];
+  let players: PLAYER[] = [
+    {ID: 1, Name: 'Midhun', DoB: 41, Gender: "M", Flat: "B4514"},
+    {ID: 2, Name: 'Anil', DoB: 41, Gender: "M", Flat: "B4514"},
+    {ID: 3, Name: 'Chandru', DoB: 41, Gender: "M", Flat: "B4514"},
+    {ID: 4, Name: 'Chetan', DoB: 41, Gender: "M", Flat: "B4514"},
+    {ID: 5, Name: 'Aniket', DoB: 42, Gender: "M", Flat: "B4514" }
+  ];
+    
 @Injectable({
   providedIn: 'root'
 })
@@ -26,8 +28,14 @@ export class PlayerService {
     return players;
   }
 
-  addPlayer = (player:PLAYER) => {
-    players.push(player)
+  addPlayer = (player: PLAYER) => {    
+    let id = { 'ID': players.length + 1}  
+
+    let valueToSave = {
+      ...player,
+      ...id
+    }
+    players.push(valueToSave)
   }
 
 }
