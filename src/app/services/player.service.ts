50bbfc55cb140f29465a6f05bc51671433efadcd
anirudhka00 <93419632+anirudhka00@users.noreply.github.com>
2021-10-30 15:50:11 +0530

    Updated players&tournament services

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -5,15 +5,16 @@ export class Players {
   'ID': number;
   'Name': string;
   'Age': number;
-  'Sex': string;
+  'Gender': string;
   'Flat': string;
 }
 
   let players: Players[] = [
-    {ID: 1, Name: 'Midhun', Age: 41, Sex: "M", Flat: "B4514"},
-    {ID: 2, Name: 'Anil', Age: 41, Sex: "M", Flat: "B4514"},
-    {ID: 3, Name: 'Chandru', Age: 41, Sex: "M", Flat: "B4514"},
-    {ID: 4, Name: 'Chetan', Age: 41, Sex: "M", Flat: "B4514"}];
+    {ID: 1, Name: 'Midhun', Age: 41, Gender: "M", Flat: "B4514"},
+    {ID: 2, Name: 'Anil', Age: 41, Gender: "M", Flat: "B4514"},
+    {ID: 3, Name: 'Chandru', Age: 41, Gender: "M", Flat: "B4514"},
+    {ID: 4, Name: 'Chetan', Age: 41, Gender: "M", Flat: "B4514"},
+    {ID: 5, Name: 'Aniket', Age: 42, Gender: "M", Flat: "B4514"}];
 @Injectable({
   providedIn: 'root'
 })
--- src/app/services/tournament.service.ts
+++ src/app/services/tournament.service.ts
@@ -4,19 +4,19 @@ import { PlayerService } from './player.service';
 export class TOURNAMENT {
   'ID': number;
   'StartTime': string;
-  'EndTime' : string;
+  'EndTime': string;
   'Team1': TEAM;
   'Team2': TEAM;
   'TossWinner': string;
   'Category': string;
-  'GameMoves': GAME_MOVES[] ;
+  'GameMoves': GAME_MOVES[];
 }
 
 export class TEAM {
   'TeamName': string;
-  'player1_ID': string;
+  'player1_ID': number;
   'player1_Name': string;
-  'player2_ID': string;
+  'player2_ID': number;
   'player2_Name': string;
 }
 
@@ -30,77 +30,85 @@ export class GAME_MOVES {
   'ServeBox': string;
 }
 
-  let Tournaments: TOURNAMENT[] = [
-    {
-      ID: 1,
-      StartTime: "2013-03-11 11:35:24",
-      EndTime: "2013-03-11 11:35:24",
-      Team1: {
-        'TeamName':"Team A",
-        'player1_ID': "string",
-        'player1_Name': "string",
-        'player2_ID': "string",
-        'player2_Name': "string"
-      },
-      Team2: {
-        'TeamName':"Team B",
-        'player1_ID': "string",
-        'player1_Name': "string",
-        'player2_ID': "string",
-        'player2_Name': "string"
-      },
-      TossWinner: "",
-      'Category': "Mens Doubles",
-      GameMoves: [{
-        'LeftTopPlayer': "",
-        'LeftBottomPlayer': "",
-        'RightTopPlayer': "",
-        'RightBottomPlayer': "",
-        'Team1Score': 0,
-        'Team2Score': 0,
-        'ServeBox': "LeftBottom",
-      },]
+let Tournaments: TOURNAMENT[] = [
+  {
+    ID: 1,
+    StartTime: "2013-03-11 11:35:24",
+    EndTime: "2013-03-11 11:35:24",
+    Team1: {
+      'TeamName': "Team A",
+      'player1_ID': 1,
+      'player1_Name': "Midhun",
+      'player2_ID': 2,
+      'player2_Name': "Anil"
     },
-    {
-      ID: 2,
-      StartTime: "2013-03-11 11:35:24",
-      EndTime: "2013-03-11 11:35:24",
-      Team1: {
-        'TeamName':"Team A",
-        'player1_ID': "string",
-        'player1_Name': "string",
-        'player2_ID': "string",
-        'player2_Name': "string"
-      },
-      Team2: {
-        'TeamName':"Team B",
-        'player1_ID': "string",
-        'player1_Name': "string",
-        'player2_ID': "string",
-        'player2_Name': "string"
-      },
-      TossWinner: "",
-      'Category': "Mens Doubles",
-      GameMoves: [{
-        'LeftTopPlayer': "",
-        'LeftBottomPlayer': "",
-        'RightTopPlayer': "",
-        'RightBottomPlayer': "",
-        'Team1Score': 0,
-        'Team2Score': 0,
-        'ServeBox': "",
-      },]
+    Team2: {
+      'TeamName': "Team B",
+      'player1_ID': 3,
+      'player1_Name': "Chandru",
+      'player2_ID': 4,
+      'player2_Name': "Chetan"
     },
-  ]
+    TossWinner: "Team B",
+    'Category': "Mens Doubles",
+    GameMoves: [{
+      'LeftTopPlayer': "Midhun",
+      'LeftBottomPlayer': "Anil",
+      'RightTopPlayer': "Chandru",
+      'RightBottomPlayer': "Chetan",
+      'Team1Score': 0,
+      'Team2Score': 0,
+      'ServeBox': "RightTop",
+    }, {
+      'LeftTopPlayer': "Midhun",
+      'LeftBottomPlayer': "Anil",
+      'RightTopPlayer': "Chetan",
+      'RightBottomPlayer': "Chandru",
+      'Team1Score': 0,
+      'Team2Score': 1,
+      'ServeBox': "RightBottom",
+    },]
+  },
+  {
+    ID: 2,
+    StartTime: "2013-03-11 11:35:24",
+    EndTime: "2013-03-11 11:35:24",
+    Team1: {
+      'TeamName': "Team B",
+      'player1_ID': 3,
+      'player1_Name': "Chandru",
+      'player2_ID': 4,
+      'player2_Name': "Chetan"
+    },
+    Team2: {
+      'TeamName': "Team A",
+      'player1_ID': 1,
+      'player1_Name': "Midhun",
+      'player2_ID': 2,
+      'player2_Name': "Anil"
+    },
+    TossWinner: "",
+    'Category': "Mens Doubles",
+    GameMoves: [{
+      'LeftTopPlayer': "",
+      'LeftBottomPlayer': "",
+      'RightTopPlayer': "",
+      'RightBottomPlayer': "",
+      'Team1Score': 0,
+      'Team2Score': 0,
+      'ServeBox': "",
+    },]
+  },
+]
 @Injectable({
   providedIn: 'root'
 })
 export class TournamentService {
   selectedTournament: any
   constructor(private playerService: PlayerService,) { }
-  
+
   getTournament = (ID: any) => {
-    var result = Tournaments.find(obj => {return obj.ID === ID})
+    var result = Tournaments.find(obj => { return obj.ID === ID })
     this.selectedTournament = result;
     console.log(this.selectedTournament)
     return result;
